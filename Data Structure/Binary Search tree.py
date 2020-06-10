4ca8aae7e67277c509402afa78b04d15d7afee45
kanuka goel <65423384+kanuka21@users.noreply.github.com>
2020-06-10 17:32:20 +0530

    Create Binary Search tree.py

---
+++ Data Structure/Binary Search tree.py
@@ -0,0 +1,20 @@
+
+
+
+
+def lca(root, v1, v2):
+    # compare v1 v2 and get the big and small value
+    if v1 > v2:
+        v_big, v_small = v1, v2
+    else:
+        v_big, v_small = v2, v1
+
+    current = root
+    while True:
+        if current.info > v_big:
+            current = current.left
+        elif current.info < v_small:
+            current = current.right
+        elif current.info >= v_small and current.info <= v_big:
+            return current
+  
