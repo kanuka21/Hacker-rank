595fab3a101097346d6c5b03f53f825dafd13526
kanuka goel <65423384+kanuka21@users.noreply.github.com>
2020-06-22 15:00:43 +0530

    Create SparseArray.py

---
+++ Data Structure/SparseArray.py
@@ -0,0 +1,24 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+no_strings = int(input())
+strings = []
+no_q = 0
+queries = []
+
+for _ in range(no_strings):
+    strings.append(input())
+
+no_q = int(input())
+for _ in range(no_q):
+    queries.append(re.compile(r'^'+input()+'$'))
+
+for pattern in queries:
+    found = 0
+    for s in strings:
+        found += len(pattern.findall(s))
+    print(found)
